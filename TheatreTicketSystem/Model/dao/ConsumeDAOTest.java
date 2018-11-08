d47700740b6ea130945a9d6af7320488cca08f3e
Ddd <752042257@qq.com>
2018-11-08 10:51:21 +0800

    hgx_config

--- TheatreTicketSystem/Model/dao/ConsumeDAOTest.java
+++ TheatreTicketSystem/Model/dao/ConsumeDAOTest.java
@@ -3,6 +3,10 @@ package dao;
 import static org.junit.Assert.*;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.math.BigDecimal;
 
 import org.junit.After;
@@ -24,13 +28,15 @@ public class ConsumeDAOTest extends TestCase {
 	public void setUp() throws Exception {
 		super.setUp();
 		dbc = new DatabaseConnection();
+		//dbc.runSQL("/Users/ddd/Documents/SJTU_Courses/SoftwareTest/Software_Test_2018Fall/TheatreTicketSystem/resources/sample.sql");
 		consumeDAO = new ConsumeDAO();
+		consumeDAO.runSQL("/Users/ddd/Documents/SJTU_Courses/SoftwareTest/Software_Test_2018Fall/TheatreTicketSystem/resources/sample.txt");
 		Connection conn = dbc.getConnection();
     	System.out.println("consume conn success");
     	member_expect = new Member();
     	member_expect.setMemberId("A0001");
-    	member_expect.setAccount(BigDecimal.valueOf(200000,2));
-    	member_expect.setConsumption(BigDecimal.valueOf(200000,2));
+    	member_expect.setAccount(BigDecimal.valueOf(100000,2));
+    	member_expect.setConsumption(BigDecimal.valueOf(500000,2));
 	}
 
 	@After
@@ -50,10 +56,10 @@ public class ConsumeDAOTest extends TestCase {
 
 	@Test
 	public void testRecharge() {
-		assertTrue(consumeDAO.recharge("A0001", BigDecimal.valueOf(20000,2)));
+		assertTrue(consumeDAO.recharge("A0001", BigDecimal.valueOf(2000000,2)));
 		assertFalse(consumeDAO.recharge("A0001", null));
-		assertFalse(consumeDAO.recharge("A0001", BigDecimal.valueOf(-10000.2)));
-		assertFalse(consumeDAO.recharge("A0002", BigDecimal.valueOf(20000,2)));
+		assertFalse(consumeDAO.recharge("A0001", BigDecimal.valueOf(-200000,2)));
+		assertFalse(consumeDAO.recharge("A0002", BigDecimal.valueOf(400000,2)));
 		assertFalse(consumeDAO.recharge("A0002",null));
 		assertFalse(consumeDAO.recharge("A0002",BigDecimal.valueOf(-10000.2)));
 		assertFalse(consumeDAO.recharge(null,BigDecimal.valueOf(20000,2)));
@@ -63,30 +69,30 @@ public class ConsumeDAOTest extends TestCase {
 
 	@Test
 	public void testPurchaseByMember() {
-		assertTrue(consumeDAO.purchaseByMember("念念手记", "A0001", 2, "A0001"));
+		assertTrue(consumeDAO.purchaseByMember("念念手记", "A0001", 3, "A0001"));
 		assertFalse(consumeDAO.purchaseByMember(null, "A0001", 3, "A0001"));
 		assertFalse(consumeDAO.purchaseByMember("念念手记", null, 4, "A0001"));
 		assertFalse(consumeDAO.purchaseByMember("念念手记", "A0001", -1, "A0001"));
-		assertFalse(consumeDAO.purchaseByMember("念念手记", "A0001", 41, "A0001"));
+		assertFalse(consumeDAO.purchaseByMember("念念手记", "A0001", 201, "A0001"));
 		assertFalse(consumeDAO.purchaseByMember("念念手记", "A0001", 5, null));
 		assertFalse(consumeDAO.purchaseByMember(null, null, 6, "A0001"));
 		assertFalse(consumeDAO.purchaseByMember(null, "A0001", -1, "A0001"));
-		assertFalse(consumeDAO.purchaseByMember(null, "A0001", 41, "A0001"));
+		assertFalse(consumeDAO.purchaseByMember(null, "A0001", 201, "A0001"));
 		assertFalse(consumeDAO.purchaseByMember(null, "A0001", 7, null));
 		assertFalse(consumeDAO.purchaseByMember("念念手记", null, -1, "A0001"));
-		assertFalse(consumeDAO.purchaseByMember("念念手记", "A0001", 41, "A0001"));
+		assertFalse(consumeDAO.purchaseByMember("念念手记", "A0001", 201, "A0001"));
 		assertFalse(consumeDAO.purchaseByMember("念念手记", "A0001", 8, null));
 		assertFalse(consumeDAO.purchaseByMember("念念手记", "A0001", -1, null));
-		assertFalse(consumeDAO.purchaseByMember("念念手记", null, 41, null));
+		assertFalse(consumeDAO.purchaseByMember("念念手记", null, 201, null));
 		assertFalse(consumeDAO.purchaseByMember(null, null, -1, "A0001"));
-		assertFalse(consumeDAO.purchaseByMember(null, null, 41, "A0001"));
+		assertFalse(consumeDAO.purchaseByMember(null, null, 201, "A0001"));
 		assertFalse(consumeDAO.purchaseByMember("念念手记", "A0001", -1, null));
-		assertFalse(consumeDAO.purchaseByMember("念念手记", "A0001", 41, null));
+		assertFalse(consumeDAO.purchaseByMember("念念手记", "A0001", 201, null));
 		assertFalse(consumeDAO.purchaseByMember(null, "A0001", -1, null));
-		assertFalse(consumeDAO.purchaseByMember(null, "A0001", 41, null));
+		assertFalse(consumeDAO.purchaseByMember(null, "A0001", 201, null));
 		assertFalse(consumeDAO.purchaseByMember(null, null, 9, null));
 		assertFalse(consumeDAO.purchaseByMember(null, null, -1, null));
-		assertFalse(consumeDAO.purchaseByMember(null, null, 41, null));
+		assertFalse(consumeDAO.purchaseByMember(null, null, 201, null));
 	}
 
 }
--- TheatreTicketSystem/Model/dbc/DatabaseConnection.java
+++ TheatreTicketSystem/Model/dbc/DatabaseConnection.java
@@ -1,14 +1,18 @@
 package dbc;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
 	private static final String DBDRIVER = "com.mysql.cj.jdbc.Driver";
 	private static final String DBURL = "jdbc:mysql://localhost:3306?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=Asia/Shanghai";
-	private static final String DBuser = "theatre_admin";
-	private static final String DBPASSWORD = "";
+	private static final String DBuser = "root";
+	private static final String DBPASSWORD = "qwer1234";
 	private Connection conn = null;
 	
 	public DatabaseConnection() throws Exception {
@@ -35,4 +39,24 @@ public class DatabaseConnection {
             this.conn.close();
         }
     }
+    
+    public boolean runSQL(String sqlFileName) {
+		boolean flag = true;
+		File file = new File(sqlFileName);
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			String sql = null;
+			while((sql = reader.readLine()) != null) {
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				int rows_count = pstmt.executeUpdate();
+				System.out.println(rows_count + ":\t" + sql);
+			}
+			reader.close();
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+			flag = false;
+		}
+		return flag;
+	}
 }
